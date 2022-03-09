284fc311a1cb992fa7881e4d0aa209d196a6482c
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2022-03-09 12:41:19 +0200

    delete method changed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -57,13 +57,11 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Deletes obj from __objects if it's inside
-        """
-        if obj:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            if key in self.__objects.keys():
+        """Method that deletes obj from __objects"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
                 del self.__objects[key]
-                self.save()
 
     def close(self):
         """Call the reload method."""
