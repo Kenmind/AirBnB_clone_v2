817aaf3f5dabaeb2c7874c4af5c89803094760aa
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2022-03-09 21:42:21 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -44,41 +44,55 @@ class Place(BaseModel, Base):
         amenity_ids (list): An id list of all linked amenities.
     """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
-    amenity_ids = []
+        if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=False)
+        longitude = Column(Float, nullable=False)
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False)
+        amenity_ids = []
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
 
-    if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
         def reviews(self):
-            """Get a list of all linked Reviews."""
-            review_list = []
-            for review in list(models.storage.all(Review).values()):
-                if review.place_id == self.id:
-                    review_list.append(review)
-            return review_list
+            """returns list of Reviews instances"""
+            res = []
+            for i in models.storage.all(Review).values():
+                if i.place_id == self.id:
+                    res.append(i)
+            return res
 
         @property
         def amenities(self):
-            """Get/set linked Amenities."""
-            amenity_list = []
-            for amenity in list(models.storage.all(Amenity).values()):
-                if amenity.id in self.amenity_ids:
-                    amenity_list.append(amenity)
-            return amenity_list
+            """returns list of amenity instances"""
+            res = []
+            for i in models.storage.all(Amenity).values():
+                if i.id == self.amenity_ids:
+                    res.append(i)
+            return res
 
         @amenities.setter
         def amenities(self, value):
-            if type(value) == Amenity:
+            """setter for amenities"""
+            if isinstance(value, Amenity):
                 self.amenity_ids.append(value.id)
+            else:
+                pass
