be961e712d2a5c05069f408257ea260df0f103a0
Kennedy Kalaluka <62250484+Kenmind@users.noreply.github.com>
2022-03-09 20:40:21 +0200

    Update user.py
    User table creation

--- models/user.py
+++ models/user.py
@@ -18,16 +18,11 @@ class User(BaseModel, Base):
         last_name (sqlalchemy String): The user's last name.
         places (sqlalchemy relationship): The User-Place relationship.
         reviews (sqlalchemy relationship): The User-Review relationship.
-    """
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", cascade="all", backref="user")
-        reviews = relationship("Review", cascade="all", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """"
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all", backref="user")
+    reviews = relationship("Review", cascade="all", backref="user")
