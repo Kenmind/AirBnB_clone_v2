cdcf064bc8f33f8044a75b63f73b0c53eb44f36c
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2022-03-09 21:31:00 +0200

    re_modified

--- models/user.py
+++ models/user.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
-"""Defines the User class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+"""This is the user class"""
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
-
-class User(BaseModel, Base):
-    """Represents a user for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table users.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
-    """
-    __tablename__ = "users"
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+s = "HBNB_TYPE_STORAGE"
+if cs in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class User(BaseModel, Base):
+        """This is the class for user
+        Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+        """
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", cascade="all", backref="user")
-        reviews = relationship("Review", cascade="all", backref="user")
-    else:
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for i, j in kwargs.items():
+                setattr(self, i, j)
+else:
+    class User(BaseModel):
+        """This is the class for user
+        Attributes:
+        """
         email = ""
         password = ""
         first_name = ""
