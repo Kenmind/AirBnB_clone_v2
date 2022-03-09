b619e5d310ffb22ac0711323b95ed31318cfa28b
Kennedy Kalaluka <62250484+Kenmind@users.noreply.github.com>
2022-03-09 20:49:16 +0200

    Update user.py
    DB user by default

--- models/user.py
+++ models/user.py
@@ -20,15 +20,9 @@ class User(BaseModel, Base):
         reviews (sqlalchemy relationship): The User-Review relationship.
     """
     __tablename__ = "users"
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", cascade="all", backref="user")
-        reviews = relationship("Review", cascade="all", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all", backref="user")
+    reviews = relationship("Review", cascade="all", backref="user")
