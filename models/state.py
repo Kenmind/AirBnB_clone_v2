6d3fd1aa9d0c799ade25e588d848a400c972d145
Kenmind <kennedykalaluka04@gmail.com>
2022-03-10 08:55:05 +0200

    Fixed state class

--- models/state.py
+++ models/state.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+"""Defines the State class."""
+import models
+from os import getenv
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
-from sqlalchemy import ForeignKey
+from models.city import City
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel, Base):
-    """Represents a review for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Reviews.
-        text (sqlalchemy String): The review description.
-        place_id (sqlalchemy String): The review's place id.
-        user_id (sqlalchemy String): The review's user id.
-    """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+class State(BaseModel, Base):
+    """Represents a State"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related Cities."""
+            city_l = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_l.append(city)
+            return city_l
