9639f91887ee945f940203509457f37a847c724f
Kenmind <kennedykalaluka04@gmail.com>
2022-03-10 08:59:50 +0200

    Fixed user class

--- models/user.py
+++ models/user.py
@@ -22,7 +22,7 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", cascade="all", backref="user")
     reviews = relationship("Review", cascade="all", backref="user")
